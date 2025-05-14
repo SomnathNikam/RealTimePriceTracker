c7442e225ad6b55b992ec42f5ad86210d3ac0a1f
SOMNATH <somnathnikam2076@gmail.com>
2025-05-14 14:04:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/task/realtimepricetracker/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.task.realtimepricetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.task.realtimepricetracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
